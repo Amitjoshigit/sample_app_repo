a6e14b197b488b0c289e9997e15ca68052a7b2a7
Amitjoshigit <141041637+Amitjoshigit@users.noreply.github.com>
2025-10-15 15:21:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div style={{ textAlign: 'center', padding: 32 }}>
       <h1>Backstage App</h1>
-      <p>This is a sample React app deployed to EKS via ArgoCD.</p>
+      <p>This is a sample  STATIC React app deployed to EKS via ArgoCD .</p>
 
       {/* <p>Deployed to EKS via Argo CD.</p> */}
     </div>
